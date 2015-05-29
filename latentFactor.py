f728c9b9cd958bf82b3e3fe918ea1a6baac756da
Glenn Qian <qianjy1990@gmail.com>
2015-05-29 00:51:00 -0400

    integrated two search methods

--- latentFactor.py
+++ latentFactor.py
@@ -14,26 +14,45 @@ def read_and_join(in_path=os.curdir):
     return joined
 
 
-def do_train(df):
-    train, valid = gl.recommender \
-                     .util.random_split_by_user(df,
-                                                user_id='username',
-                                                item_id='course_id',
-                                                item_test_proportion=0.2,
-                                                max_num_users=None,
-                                                random_seed=2345)
+def do_train_rand(train, valid):
     params = {'user_id': ['username'], 'item_id': ['course_id'],
               'target': ['label'], 'binary_target': [True],
               'num_factors': stats.randint(4, 512),
               'regularization': stats.expon(scale=1.0/100000),
               'linear_regularization': stats.expon(scale=1.0/100000000)}
-    job = gl.toolkits.model_parameter_search \
-                     .random_search.create((train, valid),
-                                           gl.recommender.
-                                           factorization_recommender.create,
-                                           params)
-    print job.get_results()
-    print job.get_best_params()
+    try:
+        job = gl.toolkits.model_parameter_search \
+                         .random_search.create((train, valid),
+                                               gl.recommender.
+                                               factorization_recommender.create,
+                                               params)
+        res = job.get_results()
+        res = res.sort('validation_rmse')
+        print res[0]
+        res.save('train_history.csv', format='csv')
+    except:
+        print job.get_metrics()
+    return job
+
+
+def do_train_grid(train, valid):
+    params = {'user_id': ['username'], 'item_id': ['course_id'],
+              'target': ['label'], 'binary_target': [True],
+              'num_factors': [2 ** i for i in range(3, 7)],
+              'regularization': [10 ** i for i in range(-6, -1)],
+              'linear_regularization': [10 ** i for i in range(-10, -5)]}
+    try:
+        job = gl.toolkits.model_parameter_search \
+                         .grid_search.create((train, valid),
+                                             gl.recommender.
+                                             factorization_recommender.create,
+                                             params)
+        res = job.get_results()
+        res = res.sort('validation_rmse')
+        print res[0]
+        res.save('train_history.csv', format='csv')
+    except:
+        print job.get_metrics()
     return job
 
 
@@ -42,9 +61,15 @@ def main(args):
         data = gl.SFrame(read_and_join(str(args[0])))
     else:
         data = gl.SFrame(read_and_join())
-    job = do_train(data)
-    with open('best_params.log', 'wb') as f:
-        f.write(str(job))
+    train, valid = gl.recommender \
+                     .util.random_split_by_user(data,
+                                                user_id='username',
+                                                item_id='course_id',
+                                                item_test_proportion=0.2,
+                                                max_num_users=None,
+                                                random_seed=2345)
+
+    do_train_grid(train, valid)
 
 
 if __name__ == '__main__':
