f47b9b0fddd20a9b449cdd78f066e7eefdffbec8
Glenn Qian <qianjy1990@gmail.com>
2015-05-30 17:12:07 -0400

    minor fix

--- preproc/aggregate_counts.py
+++ preproc/aggregate_counts.py
@@ -14,7 +14,7 @@ def aggregate_naive(df):
         temp = pd.Series([df.date - pd.DateOffset()] + [0] * 9, index=df.index)
         avg_delta = pd.DataFrame([temp, df]).diff().mean().add_suffix('_delta')
     else:
-        days_span = (df.date.iloc[-1] - df.date.iloc[0]).days
+        days_span = (df.date.iloc[-1] - df.date.iloc[0]).days + 1
         days_present = df.shape[0]
         avg = df.drop('date', axis=1).mean()
         avg_delta = df.diff().mean().add_suffix('_delta')
@@ -30,9 +30,12 @@ def read_and_aggr(in_path=os.curdir):
     fn = os.path.join(in_path, 'event_counts_nz.csv')
     df = pd.read_csv(fn, header=0, index_col=0, parse_dates=['date'])
     res = []
-    for i in df.index.unique():
-        res.append(aggregate_naive(df.loc[i]))
-    return pd.DataFrame(res, index=df.index)
+    l = len(df.index.unique())
+    for i, e in enumerate(df.index.unique()):
+        res.append(aggregate_naive(df.loc[e]))
+        print 'finished {} of {}\r'.format(i+1, l),
+    print 'finished {} of {}'.format(l, l)
+    return pd.DataFrame(res, index=df.index.unique())
 
 
 def main(args):
@@ -41,6 +44,7 @@ def main(args):
     else:
         data = read_and_aggr()
     data.to_csv('aggregated_counts.csv', index_label='enrollment_id')
+    print 'done'
 
 
 if __name__ == '__main__':
